eeb438fd44c502582d3f131e0f4ed37ce110dd35
peskovs <ap24105@edu.lu.lv>
2025-06-06 10:41:57 +0300

    Izveidota DB

---
+++ smarttransport/users/admin.py
@@ -0,0 +1,14 @@
+# users/admin.py
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'loma', 'is_staff')
+    list_filter = ('loma', 'is_staff', 'is_superuser', 'is_active')
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {'fields': ('loma', 'talrunis', 'registracijas_datums')}),
+    )
+    readonly_fields = ('registracijas_datums',)
+
+admin.site.register(User, CustomUserAdmin)
