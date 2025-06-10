d11b8dbf63f32104d521313d2aa9bc4db42bf42e
meiz <meiz.mzk@gmail.com>
2025-06-10 22:50:55 +0300

    Atļāvu visus hostus

--- smarttransport/smarttransport/settings.py
+++ smarttransport/smarttransport/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-)n$_^wm-kjnmh&2v$!qipksxq5*_ffl3dnj#^g+#7%3z48o(@#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
