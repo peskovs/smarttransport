6aec6e711e13bed3da8e0288f39b943cd054f532
meiz <meiz.mzk@gmail.com>
2025-06-10 22:56:40 +0300

    Izmaiņas static failu servēšanā

--- smarttransport/smarttransport/settings.py
+++ smarttransport/smarttransport/settings.py
@@ -74,9 +74,7 @@ TEMPLATES = [
 ]
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 WSGI_APPLICATION = 'smarttransport.wsgi.application'
 
